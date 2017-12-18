9d3ef48b124c677410f258f1c7ec06cc8bd76079
Matthias Loibl <mail@matthiasloibl.com>
2017-12-18 02:00:04 +0100

    Use ipdata to get the timezone of a client

--- main.go
+++ main.go
@@ -5,17 +5,30 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/theckman/go-ipdata"
 )
 
 func main() {
-	http.HandleFunc("/", kitchen)
+	ipd := ipdata.NewClient("")
+
+	http.HandleFunc("/", kitchen(ipd))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func kitchen(w http.ResponseWriter, r *http.Request) {
-	ip := r.Header.Get("x-forwarded-for")
-	fmt.Println(ip)
+func kitchen(ipd ipdata.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		f := r.Header.Get("x-forwarded-for")
+		loc := time.UTC
 
-	kitchen := time.Now().Format(time.Kitchen)
-	fmt.Fprint(w, kitchen)
+		ip, err := ipd.Lookup(f)
+		if err == nil {
+			loc = ip.TimeZone
+		}
+
+		kitchen := time.Now().In(loc).Format(time.Kitchen)
+
+		log.Printf("%s is in timezone %s: %s\n", f, loc.String(), kitchen)
+		fmt.Fprint(w, kitchen)
+	}
 }
