db6bce1543c4a84040faa06c04bd2617f932b4e8
Matthias Loibl <mail@matthiasloibl.com>
2017-12-18 11:11:20 +0100

    Use sync.Map for caching IP to timezones

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/theckman/go-ipdata"
@@ -12,18 +13,32 @@ import (
 func main() {
 	ipd := ipdata.NewClient("")
 
-	http.HandleFunc("/", kitchen(ipd))
+	var timezones sync.Map
+
+	http.HandleFunc("/", kitchen(ipd, timezones))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func kitchen(ipd ipdata.Client) http.HandlerFunc {
+func kitchen(ipd ipdata.Client, timezones sync.Map) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		f := r.Header.Get("x-forwarded-for")
 		loc := time.UTC
 
-		ip, err := ipd.Lookup(f)
-		if err == nil {
-			loc = ip.TimeZone
+		f := r.Header.Get("x-forwarded-for")
+		if f != "" {
+			tz, ok := timezones.Load(f)
+			if ok {
+				loc = tz.(*time.Location)
+				log.Printf("loaded %s from timezones with %s\n", f, loc.String())
+			} else {
+				ip, err := ipd.Lookup(f)
+				if err != nil {
+					log.Println("failed to lookup ip", f, err)
+				} else {
+					loc = ip.TimeZone
+					timezones.Store(f, loc)
+					log.Printf("stored %s in timezones with %s\n", f, loc.String())
+				}
+			}
 		}
 
 		kitchen := time.Now().In(loc).Format(time.Kitchen)
