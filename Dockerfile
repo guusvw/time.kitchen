d0b2aecd35586e14aaf66324b30289cda507e2b4
Guus van Weelden <guus@loodse.com>
2018-05-06 12:28:58 +0200

    Draft create

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,15 @@
-FROM alpine
 
-RUN apk add --no-cache --update ca-certificates
+FROM golang:1.10 AS build-env
+
+ADD . /go/src/github.com/guusvw/time.machine
+WORKDIR /go/src/github.com/guusvw/time.machine
+
+RUN  go build -o goapp
+
+FROM scratch
 
 EXPOSE 8080
-ADD ./time.kitchen /usr/bin/time.machine
+
+COPY --from=build-env /go/src/github.com/guusvw/time.machine/goapp /usr/bin/time.machine
 
 ENTRYPOINT ["/usr/bin/time.machine"]
--- main.go
+++ main.go
@@ -1,49 +1,77 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"io"
 	"log"
 	"net/http"
-	"sync"
-	"time"
+)
 
-	"github.com/theckman/go-ipdata"
+const (
+	certificateHeaderName string = "Cert"
+)
+
+var (
+	upstreamHost = flag.String("upstream", "httpbin.org", "name of my upstream serive")
 )
 
 func main() {
-	ipd := ipdata.NewClient("")
-
-	var timezones sync.Map
-
-	http.HandleFunc("/", kitchen(ipd, timezones))
+	flag.Parse()
+	http.HandleFunc("/", checkHeader)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func kitchen(ipd ipdata.Client, timezones sync.Map) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		loc := time.UTC
-
-		f := r.Header.Get("x-forwarded-for")
-		if f != "" {
-			tz, ok := timezones.Load(f)
-			if ok {
-				loc = tz.(*time.Location)
-				log.Printf("loaded %s from timezones with %s\n", f, loc.String())
-			} else {
-				ip, err := ipd.Lookup(f)
-				if err != nil {
-					log.Println("failed to lookup ip", f, err)
-				} else {
-					loc = ip.TimeZone
-					timezones.Store(f, loc)
-					log.Printf("stored %s in timezones with %s\n", f, loc.String())
-				}
-			}
-		}
-
-		kitchen := time.Now().In(loc).Format(time.Kitchen)
-
-		log.Printf("%s is in timezone %s: %s\n", f, loc.String(), kitchen)
-		fmt.Fprint(w, kitchen)
+func checkHeader(w http.ResponseWriter, r *http.Request) {
+	cert, ok := r.Header[certificateHeaderName]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("No cert!"))
+		return
 	}
+
+	if len(cert) != 1 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("too many certs!"))
+		return
+	}
+
+	log.Printf(cert[0])
+
+	upstreamUrl := r.URL
+	upstreamUrl.Host = *upstreamHost
+	upstreamUrl.Scheme = "http"
+	log.Printf("Upstream URL: %s", upstreamUrl.String())
+
+	req, err := http.NewRequest(r.Method, upstreamUrl.String(), r.Body)
+	if err != nil {
+		log.Printf("Error in creating http.Request: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	req.Header = r.Header
+	req.Header.Del(certificateHeaderName)
+
+	client := &http.Client{
+		CheckRedirect: redirectPolicyFunc,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("Couldn't call upstream service: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	for k, vs := range resp.Header {
+		for _, v := range vs {
+			w.Header().Set(k, v)
+		}
+	}
+
+	io.Copy(w, resp.Body)
+}
+
+func redirectPolicyFunc(r *http.Request, rs []*http.Request) error {
+	return http.ErrUseLastResponse
 }
